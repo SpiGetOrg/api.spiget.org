e7f5c771ea4535988d49154db8d0fc07f6ad21c1
inventivetalent <mail@inventivetalent.org>
2021-03-09 11:18:55 +0100

    .

--- routes/metrics.js
+++ routes/metrics.js
@@ -8,53 +8,65 @@ module.exports = function (express, config) {
         let minutes = days * 1440;
 
         let startTime = new Date(Date.now() - (minutes * 60 * 1000));
+        let y = `${ startTime.getFullYear() }`;
+        let m = `${ startTime.getMonth() + 1 }`;
+        if (m.length < 2) {
+            m = "0" + m;
+        }
+        let d = `${ startTime.getDate() }`;
+        if (d.length < 2) {
+            d = "0" + d;
+        }
         axios.post("https://api.cloudflare.com/client/v4/graphql/", {
-            query: "{\n" +
-                "  viewer {\n" +
-                "    zones(filter: { zoneTag: \"" + config.cloudflare.zone + "\" }) {\n" +
-                "      httpRequests1dGroups(\n" +
-                "        orderBy: [date_ASC]\n" +
-                "        limit: 1000\n" +
-                `"        filter: { datetime_gt: \"${ startTime.getFullYear() }-${ startTime.getMonth() + 1 }-${ startTime.getDate() }\" }\n"` +
-                "      ) {\n" +
-                "        date: dimensions {\n" +
-                "          date\n" +
-                "        }\n" +
-                "        sum {\n" +
-                "          cachedRequests\n" +
-                "          requests\n" +
-                "        }\n" +
-                "        uniq {\n" +
-                "          uniques\n" +
-                "        }\n" +
-                "      }\n" +
-                "    }\n" +
-                "  }\n" +
-                "}",
+            query: '{' +
+                '  viewer {' +
+                `    zones(filter: { zoneTag: "${ config.cloudflare.zone }" }) {` +
+                '      httpRequests1dGroups(' +
+                '        orderBy: [date_ASC]' +
+                '        limit: 1000' +
+                `        filter: { date_gt: "${ y }-${ m }-${ d }" }` +
+                '      ) {' +
+                '        date: dimensions {' +
+                '          date' +
+                '        }' +
+                '        sum {' +
+                '          cachedRequests' +
+                '          requests' +
+                '        }' +
+                '        uniq {' +
+                '          uniques' +
+                '        }' +
+                '      }' +
+                '    }' +
+                '  }' +
+                '}',
             variables: {}
         }, {
             headers: {
                 "X-Auth-Email": config.cloudflare.email,
-                "X-Auth-Key": config.cloudflare.key
+                "X-Auth-Key": config.cloudflare.key,
+                "Content-Type": "application/json"
             }
-        }).then(response=>{
-            let arr = response.data.data.viewer.zones[0].httpRequests1dGroups;
-
+        }).then(response => {
             let data = [];
-            for(let entry of arr) {
-                data.push({
-                    time: entry.date.date,
-                    timestamp: Date.parse(entry.date.date),
-                    total: entry.sum.requests,
-                    unique: entry.uniq.unique
-                })
+            if (response.data.data) {
+                let arr = response.data.data.viewer.zones[0].httpRequests1dGroups;
+
+                for (let entry of arr) {
+                    data.push({
+                        time: entry.date.date,
+                        timestamp: Date.parse(entry.date.date),
+                        total: entry.sum.requests,
+                        unique: entry.uniq.uniques
+                    })
+                }
             }
 
             res.json(data);
         }).catch(err => {
             console.log(err);
             res.status(500);
-        })
+        });
     });
 
     return router;
