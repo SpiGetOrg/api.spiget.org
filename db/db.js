4498f9e108f27859cc8d938a81184af941e595e5
inventivetalent <mail@inventivetalent.org>
2021-06-18 23:56:19 +0200

    url

--- db/db.js
+++ db/db.js
@@ -13,13 +13,16 @@ module.exports = function (mongoose, config) {
         connectMongo(mongoose, config);
     }
 
-
 };
 
 function connectMongo(mongoose, config) {
-    // Connect to DB
-    console.log("Connecting to mongodb://" + ((config.mongo.user || "admin") + ":*****" + "@" + (config.mongo.address || "localhost") + ":" + (config.mongo.port || 27017) + "/" + (config.mongo.database || "database")));
-    mongoose.connect("mongodb://" + ((config.mongo.user || "admin") + ":" + (config.mongo.pass || "admin") + "@" + (config.mongo.address || "localhost") + ":" + (config.mongo.port || 27017) + "/" + (config.mongo.database || "database")));
+    if (config.mongo.url) {
+        console.log("Connecting to mongodb...");
+        mongoose.connect(config.mongo.url);
+    } else {
+        console.log("Connecting to mongodb://" + ((config.mongo.user || "admin") + ":*****" + "@" + (config.mongo.address || "localhost") + ":" + (config.mongo.port || 27017) + "/" + (config.mongo.database || "database")));
+        mongoose.connect("mongodb://" + ((config.mongo.user || "admin") + ":" + (config.mongo.pass || "admin") + "@" + (config.mongo.address || "localhost") + ":" + (config.mongo.port || 27017) + "/" + (config.mongo.database || "database")));
+    }
 
     mongoose.Promise = Promise;
 }
