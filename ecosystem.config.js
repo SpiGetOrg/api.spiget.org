8ba12ca39287cbeabf93e7e320737b4948494206
inventivetalent <mail@inventivetalent.org>
2021-06-21 19:02:09 +0200

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,8 @@ module.exports = {
     apps: [{
         name: "spiget",
         script: "index.js",
+        args: ["--color", "--time"],
+        time: true,
         max_memory_restart: "300M"
     }]
 }
