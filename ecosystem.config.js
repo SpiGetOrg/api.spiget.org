389a2f3d21093ecbc165461cbe8bb817d3c608c4
Haylee Schäfer <haylee@inventivetalent.org>
2025-09-21 09:22:15 +0200

    set interpreter

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
         script: "index.js",
         args: ["--color", "--time"],
         time: true,
-        max_memory_restart: "300M"
+        max_memory_restart: "300M",
+        interpreter: "/home/inventivetalent/.nvm/versions/node/v20.19.5/bin/node"
     }]
 }
